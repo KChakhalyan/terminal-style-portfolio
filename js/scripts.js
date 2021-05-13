1ffaee1ced3bb5072ad793fc8688bd46f7b57fcf
bartinio <karenchakhalyan23@gmail.com>
2021-05-13 14:44:16 +0400

    some fine tunings

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,6 @@ const contactContent = document.querySelector('#contact-content');
 about.addEventListener('click', () => {
 	const aboutBox = new WinBox({
 		title: 'About Me',
-		background: '#00aa00',
 		width: '400px',
 		height: '400px',
 		top: 0,
@@ -14,12 +13,17 @@ about.addEventListener('click', () => {
 		bottom: 0,
 		left: 0,
 		mount: aboutContent,
+		onfocus: function () {
+			this.setBackground('#00aa00');
+		},
+		onblur: function () {
+			this.setBackground('#777');
+		},
 	});
 });
 contact.addEventListener('click', () => {
 	const contactBox = new WinBox({
 		title: 'Contact',
-		background: '#00aa00',
 		width: '400px',
 		height: '400px',
 		top: 50,
@@ -27,5 +31,11 @@ contact.addEventListener('click', () => {
 		bottom: 50,
 		left: 50,
 		mount: contactContent,
+		onfocus: function () {
+			this.setBackground('#00aa00');
+		},
+		onblur: function () {
+			this.setBackground('#777');
+		},
 	});
 });
