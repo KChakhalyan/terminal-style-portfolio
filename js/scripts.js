0a2ec7a3c37cc5482bc09784157ce77aae5f28d0
bartinio <karenchakhalyan23@gmail.com>
2021-05-13 14:39:16 +0400

    some fine tunings

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,30 @@ const about = document.querySelector('#about');
 const contact = document.querySelector('#contact');
 const aboutContent = document.querySelector('#about-content');
 const contactContent = document.querySelector('#contact-content');
+
+about.addEventListener('click', () => {
+	const aboutBox = new WinBox({
+		title: 'About Me',
+		background: '#00aa00',
+		width: '400px',
+		height: '400px',
+		top: 0,
+		right: 0,
+		bottom: 0,
+		left: 0,
+		mount: aboutContent,
+	});
+});
+contact.addEventListener('click', () => {
+	const contactBox = new WinBox({
+		title: 'Contact',
+		background: '#00aa00',
+		width: '400px',
+		height: '400px',
+		top: 50,
+		right: 50,
+		bottom: 50,
+		left: 50,
+		mount: contactContent,
+	});
+});
