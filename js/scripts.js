f21de14d64b4f39e2900c5c8e209db4036a9c03b
KChakhalyan <karenchakhalyan23@gmail.com>
2023-02-03 23:35:21 +0400

    Changes in js width size

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,41 @@
-const about = document.querySelector('#about');
-const contact = document.querySelector('#contact');
-const aboutContent = document.querySelector('#about-content');
-const contactContent = document.querySelector('#contact-content');
+const about = document.querySelector("#about");
+const contact = document.querySelector("#contact");
+const aboutContent = document.querySelector("#about-content");
+const contactContent = document.querySelector("#contact-content");
 
-about.addEventListener('click', () => {
-	const aboutBox = new WinBox({
-		title: 'About Me',
-		width: '400px',
-		height: '400px',
-		top: 0,
-		right: 0,
-		bottom: 0,
-		left: 0,
-		mount: aboutContent,
-		onfocus: function () {
-			this.setBackground('#00aa00');
-		},
-		onblur: function () {
-			this.setBackground('#777');
-		},
-	});
+about.addEventListener("click", () => {
+   const aboutBox = new WinBox({
+      title: "About Me",
+      width: "400px",
+      height: "400px",
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      mount: aboutContent,
+      onfocus: function () {
+         this.setBackground("#00aa00");
+      },
+      onblur: function () {
+         this.setBackground("#777");
+      },
+   });
 });
-contact.addEventListener('click', () => {
-	const contactBox = new WinBox({
-		title: 'Contact',
-		width: '400px',
-		height: '400px',
-		top: 50,
-		right: 50,
-		bottom: 50,
-		left: 50,
-		mount: contactContent,
-		onfocus: function () {
-			this.setBackground('#00aa00');
-		},
-		onblur: function () {
-			this.setBackground('#777');
-		},
-	});
+contact.addEventListener("click", () => {
+   const contactBox = new WinBox({
+      title: "Contact",
+      width: "500px",
+      height: "400px",
+      top: 50,
+      right: 50,
+      bottom: 50,
+      left: 50,
+      mount: contactContent,
+      onfocus: function () {
+         this.setBackground("#00aa00");
+      },
+      onblur: function () {
+         this.setBackground("#777");
+      },
+   });
 });
